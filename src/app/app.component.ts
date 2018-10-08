48b311338c01e6895f0b97c7475cf44f1b613ee7
SD95 <abckonefsidy95@gmail.com>
2018-10-08 10:54:02 +0000

    Modifications sur le AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tp1-angular-oc';
+
+  posts = [
+    {title:"Mon premier post test", content:"Je m'engage à finir ce cours de openclassroom en 24 h",loveIts: 0, created_at:new Date},
+    {title:"Mon second post", content:"Et je m'engage à finir 2 cours de coursera en 1 mois", loveIts:0, created_at:new Date()},
+    {title:"Mon dernier poste et non le moindre", content:"Bonne chance pour gérer tout ça", loveIts:0, created_at:new Date()}
+  
+  ];
 }
